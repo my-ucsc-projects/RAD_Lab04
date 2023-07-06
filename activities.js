7766ac4c21e80977040cc43304121f64d3f85f9f
Ravindu Hasanka <rhatu2000@gmail.com>
2023-07-06 23:38:36 +0530

    activity 03

--- activities.js
+++ activities.js
@@ -20,8 +20,35 @@ console.log("Area of the rectangle 1 is "+rec1.getArea());
 console.log("Area of the rectangle 2 is "+rec2.getArea());
 
 
-// --------------------------------Activity 02--------------------------------
+// --------------------------------Activity 03--------------------------------
 
+class Dog{
+    constructor(breed,age,color){
+        this.Breed = breed;
+        this.Age = age;
+        this.Color = color;
+    }
+    Eat(){
+        return this.Breed+" is eating";
+    }
+    Sleep(){
+        return this.Breed+" is sleeping";
+    }
+    Sit(){
+        return this.Breed+" is sitting";
+    }
+    Run(){
+        return this.Breed+" is running";
+    }
+}
 
+const dog1 = new Dog("Pug",3,"Black");
+const dog2 = new Dog("Boxer",2,"White");
+const dog3= new Dog("Poodle",1,"Brown");
 
-
+console.log("Color of 1st dog is "+dog1.color);
+console.log(dog1.Eat())
+console.log("Color of 2nd dog is "+dog2.color);
+console.log(dog2.Sleep())
+console.log("Color of 3rd dog is "+dog3.color);
+console.log(dog3.Sit())
